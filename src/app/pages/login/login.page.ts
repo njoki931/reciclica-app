c57eb594f05502ec6e21d7fc74cbd88f89054059
Zeinab Njoki <waina1njoki@gmail.com>
2025-05-07 14:54:00 +0300

    new updated git global configs

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,23 @@
+import { Router } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+  standalone: false,
+})
+export class LoginPage implements OnInit {
+
+  router = inject(Router);
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  register(){
+    this.router.navigate(['/register']);
+  }
+
+}
---
+++ src/app/pages/pickup-call/pickup-call.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pickup-call',
+  templateUrl: './pickup-call.page.html',
+  styleUrls: ['./pickup-call.page.scss'],
+  standalone: false,
+})
+export class PickupCallPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
